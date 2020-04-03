191650d59bc6af0d9f3cfd8c36cfa94eeeb9a0da
erik-mcleod <erik.mcleod@centurylink.com>
2020-04-03 16:59:33 +0000

    Lab 2

--- main.js
+++ main.js
@@ -69,9 +69,36 @@ function get(serviceNowTable, callback) {
     uri: `/api/now/table/${serviceNowTable}?sysparm_limit=1`,
   };
 
-  /**
+  // Send Request to ServiceNow.
+  // We are passing variable requestOptions for the first argument.
+  // We are passing an anonymous function, an error-first callback,
+  // for the second argument.
+  request(requestOptions, (error, response, body) => {
+    /**
+     * Process ServiceNow error, response and body.
+     * Check error and response code to make sure
+     * response is good.
+     */
+    if (error) {
+      console.error('Error present.');
+      callbackError = error;
+    } else if (!validResponseRegex.test(response.statusCode)) {
+      console.error('Bad response code.');
+      callbackError = response;
+    } else if (response.body.includes('Instance Hibernating page')) {
+      callbackError = 'Service Now instance is hibernating';
+      console.error(callbackError);
+    } else {
+      callbackData = response;
+    }
+    return callback(callbackData, callbackError);
+  });
+
+}
+
+/**
  * @function post
- * @description Call the ServiceNow GET API.
+ * @description Call the ServiceNow POST API.
  *
  * @param {string} serviceNowTable - The table target of the ServiceNow table API.
  * @param {iapCallback} callback - Callback a function.
@@ -90,7 +117,7 @@ function post(serviceNowTable, callback) {
   // that holds the HTTP request method, credentials, and the API's URL.
   // Some properties are hardcoded, like the method and part of the URI.
   // Some properties are read from global const options.
-  // Some properties are passed into function get() through parameters.
+  // Some properties are passed into function post() through parameters.
   const requestOptions = {
     method: 'POST',
     auth: {
@@ -127,7 +154,6 @@ function post(serviceNowTable, callback) {
   });
 
 }
-
 
 /*
  * This section is used to test your project.
